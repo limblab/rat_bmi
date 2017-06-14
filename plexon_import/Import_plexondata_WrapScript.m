b2b3a95e701bdf3c8e42b73b559096aa3cbc6210
pabloslash <tostadomarcos@gmail.com>
2017-06-14 13:37:29 -0500

    Changes to import plexon data files

--- plexon_import/Import_plexondata_WrapScript.m
+++ plexon_import/Import_plexondata_WrapScript.m
@@ -5,34 +5,43 @@ close all; clear all; home;
 %   .mat file of binned data.
 %
 % PARAMETERS:
-%   directory.rawdata     : Directory with stored .plx files
+%   directory.rawdata     : Directory with .plx files
 %   directory.database    : Directory where .mat binned files will be saved
 %   binSize               : Bin Size desired
+%   sorted                : Whether we have sorted spikes on a .plx
 %
 % OUTPUTS:
 %   nameoffile_binned.mat : the binned file! Yessss!
 %
 % TIPS:
 %
-% Written by Pablo Tostado & Maria Jantz. Updated April 2017
+% Written by Pablo Tostado & Maria Jantz. Updated May 2017
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% If the plexon routines are not already in the Path:
-plexonRoutinesPath = 'YourPath\rat_bmi\plexon_import';
+%% Add directories to path
+myPath = 'C:\Users\Pablo_Tostado\Desktop\Northwestern_University\MyWorkFolder\Analysis';
+
+% In case the plexon routines are not already in the Path:
+plexonRoutinesPath = [myPath '\rat_bmi\plexon_import'];
 addpath(genpath(plexonRoutinesPath));
 
 % Set the directories' paths:
-directory = 'YourPath\rat_bmi\';
+directory = [myPath '\rat_bmi\'];
 directories.rawdata = [directory 'Rawdata'];
 directories.database = [directory 'Database'];
 cd(directories.rawdata)
 
-%FILENAME : Be specific if you only want one certain file
-targetfile = '*21.plx'; 
-filenames = dir(targetfile);
+%% Set params
 
-% BIN SIZE: Specify desired bin size
+% PARAMS: Specify desired bin size and if file is sorted
 binSize = 0.05;
+sorted = 0; % Change to 1 if you're using sorted.plx file, if unsorted, set to 0
+
+%FILENAME : Be specific if you only want one certain file
+SPIKEtargetfile = 'A15_170512_noobstacle_1.plx';        % !Different from original file if sorted
+EMGtargetfile = 'A15_170512_noobstacle_1.plx';          % EMGs are not saved in the sorted file after offline sorting
+
+filenames = dir(SPIKEtargetfile);
 
 for fileind = 1:length(filenames)
     filename = filenames(fileind).name;
@@ -46,17 +55,44 @@ for fileind = 1:length(filenames)
     [num2str(fileind) '  ' num2str(length(filenames)) '  ' animal '  ' date];
     
     cd(directories.rawdata)
-    plexondata = load_plexondata_spikes(filename, binSize);
     
+    %Load neural data and bin it.
+    plexondata = load_plexondata_spikes(filename, binSize, sorted);
+    plexondata = create_spikeratedata_matrix (plexondata);
+    
+    %Add timeframe
+    numBins = length(plexondata.channels(1).clusters.binneddata.spikeratedata);
+    plexondata.timeframe = (0:binSize:numBins*binSize-binSize)';
+    
+    filenames = dir(EMGtargetfile);
+    filename = filenames(fileind).name;
+    
+    try
+        %Load EMG data and bin it.
+        EMG_params.binsize = binSize; EMG_params.EMG_lp = 5; EMG_params.EMG_hp = 50;
+        [emgdatabin, emgdata] = load_plexondata_EMG(filename, EMG_params); 
+        plexondata.emgdatabin = emgdatabin; plexondata.emgdata = emgdata;
+        %Name of channels. Change this line if there are actual names        
+        plexondata.emgguide = cellstr(num2str(plexondata.emgdata.channel'))'; 
+        
+        %if you want to see the data...
+        figure; plot(plexondata.emgdata.data(:,2:end) + [1:size(plexondata.emgdata.data, 2)-1]); title('Raw EMGs');
+        figure; plot(plexondata.emgdatabin + [1:size(plexondata.emgdatabin, 2)]); title('Binned EMGs');
+    catch 
+        disp('Unable to load EMG data')
+    end
     % ADD CALL TO VICON DATA. !!! This functionality has not been fixed, might not work.
     % plexondata.viconsync = load_plexon_vicondata(animal,date);
  
-    % ADD CALL TO MARIA'S EMG FUNCTION
-    % load_plexon_emgs(animal,date);
-
-    cd(directories.database)
-    save([animal '_' date '_plexondata.mat'],'plexondata')    
+    %Create folder in Database to store animal info
+    animalDir = [directories.database '\' animal '_' date];
+    if ~exist(animalDir, 'dir')
+        mkdir(animalDir)
+    end
+    
+    cd(animalDir)
+    save([animal '_' date '_binned.mat'],'plexondata')    
 end
 
 
-clear;
+%clear; clc;
--- plexon_import/Plexon_analysis_code/subroutines/load_plexondata_spikes.m
+++ plexon_import/Plexon_analysis_code/subroutines/load_plexondata_spikes.m
@@ -1,6 +1,6 @@
-function [plexondata] = load_plexondata_spikes(filename, binSize)
+function [plexondata] = load_plexondata_spikes(filename, binSize, sorted)
 
-
+% sorted =  0 (unsorted) - 1 (sorted)
 %filename = [animal '_' date '_sortedonly.plx'];
 
 % Time information: recording length and bins for calculating firing rates
@@ -8,20 +8,19 @@ function [plexondata] = load_plexondata_spikes(filename, binSize)
 
 plexondata.binneddata.binwidth       = binSize;
 plexondata.binneddata.timebinedges   = 0:plexondata.binneddata.binwidth:plexondata.recording_length_sec;
-plexondata.binneddata.timebincenters = 0.5*plexondata.binneddata.binwidth+plexondata.binneddata.timebinedges(1:end-1);
+plexondata.binneddata.timebincenters = 0.5*plexondata.binneddata.binwidth + plexondata.binneddata.timebinedges(1:end-1);
 
 [spikecountmatrix, ~] = plx_info(filename, 1);
 
 % Get cluster data
 for channel = 1:48
-    numclusters = length(find(spikecountmatrix(:,channel+1)>0));
+    numclusters = length(find(spikecountmatrix(sorted+1:end,channel+1)>0)); %If the file is sorted, sorted units are saved from the 2nd raw downwards. If it is unsorted, units are on first raw.
     %[channel numclusters]
     if numclusters > 0
             clusters = struct();
             for cluster = 1:numclusters
-                numspikes = spikecountmatrix(cluster+1,channel+1);
-       %       [cluster (numspikes)]
-                [~, clusters(cluster).spiketimes]    = plx_ts(filename, channel, cluster);
+                numspikes = spikecountmatrix(sorted+1,channel+1); % From what I saw, spikes are stored in row 1 for raw data, and in raw 2 for sorted spikes. Why?
+                [~, clusters(cluster).spiketimes]    = plx_ts(filename, channel, sorted);
                 spikehist = histc(clusters(cluster).spiketimes,plexondata.binneddata.timebinedges); spikehist(end) = [];
                 clusters(cluster).binneddata.spikeratedata = spikehist/plexondata.binneddata.binwidth;
             end
